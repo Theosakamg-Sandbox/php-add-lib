571536a5cfd0b474b40fd7088ab9849669935c88
Mickael Gaillard <mickael.gaillard@tactfactory.com>
2017-12-22 11:37:33 +0100

    Initital Commit

---
+++ AddCommand.php
@@ -0,0 +1,10 @@
+<?php
+
+class AddCommand
+{
+
+    public function add(int $a, int $b) : int {
+        return $a + $b;
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,4 @@
+php-add_lib
+===========
+
+Library of sum function.
